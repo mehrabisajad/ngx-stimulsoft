1a788aecdf5f5c99d6ac9d01032554553a04ea6b
s.shobeyri <s.shobeyri@stsepehr.ir>
2025-07-15 14:07:12 +0330

    feat: update to latest version and add support license key and file

--- src/stimulsoft/util.ts
+++ src/stimulsoft/util.ts
@@ -1,5 +1,2 @@
 let lastId = 0;
-export function UniqueComponentId(prefix = 'stimulsoft_id_') {
-  lastId++;
-  return `${prefix}${lastId}`;
-}
+export const UniqueComponentId = (prefix = 'stimulsoft_id_'): string => `${prefix}${++lastId}`;
